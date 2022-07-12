3b33fe23efdf72c80d3ad6c2ce6a610273eb950d
luisferllub230 <84060723+luisferllub230@users.noreply.github.com>
2022-07-12 16:26:11 -0400

    type: added the prefix rout

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import path from "path";
 import expressHbs from "express-handlebars";
-import routers from "./src/Routes/routers.js";
+import usersRout from "./src/Routes/users.js";
+import adminRout from "./src/Routes/admin.js";
 
 // Initialize express app
 const app = express();
@@ -20,7 +21,8 @@ app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "./src/Views"));
 
 //call the middleware
-app.use(routers);
+app.use('/user/',usersRout);
+app.use('/admin/',adminRout);
 
 app.use("/", (req, res, next) => { //no found page
     res.status(404).render("404",{
---
+++ src/Routes/admin.js
@@ -0,0 +1,10 @@
+import express from "express";
+import * as userController from "../Controllers/userController.js";
+
+//initialize router
+const rout = express.Router();
+
+//middleware
+
+
+export default rout;
