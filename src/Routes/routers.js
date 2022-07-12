41bf7acecc7a5359d716da46cae109d20efe35c3
luisferllub230 <84060723+luisferllub230@users.noreply.github.com>
2022-07-12 15:17:17 -0400

    type: update routes

--- src/Routes/routers.js
+++ src/Routes/routers.js
@@ -4,10 +4,19 @@ import express from "express";
 const rout = express.Router();
 
 //middleware
+rout.use("/genre",(req, res, next) => {
+    res.render("genre",{
+        title: "Home",
+        message: "Welcome to genre page",
+        activeGenre: true,
+    });
+});
+
 rout.use("/",(req, res, next) => {
     res.render("index",{
         title: "Home",
         message: "Welcome to the home page",
+        activeHome: true,
     });
 });
 
