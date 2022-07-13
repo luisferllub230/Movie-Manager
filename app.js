842457e451db551a84fcf3459ae44c9b997243f5
luisferllub230 <84060723+luisferllub230@users.noreply.github.com>
2022-07-12 23:52:56 -0400

    type: added bodyparse

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ import expressHbs from "express-handlebars";
 import usersRout from "./src/Routes/users.js";
 import adminRout from "./src/Routes/admin.js";
 import errorRout from "./src/Routes/error.js";
+import bodyParser from "body-parser";
 
 // Initialize express app
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //make the static folder public
 const __dirname = path.resolve();
